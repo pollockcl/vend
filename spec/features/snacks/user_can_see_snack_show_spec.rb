4d712f63aa9ade6c0a72a4aae2ccc02ae2aebd80
pollockcl <pollock.clee@gmail.com>
2018-04-06 10:49:42 -0600

    test change to match example

--- spec/features/snacks/user_can_see_snack_show_spec.rb
+++ spec/features/snacks/user_can_see_snack_show_spec.rb
@@ -4,17 +4,23 @@ context 'the user visits the snack show page' do
   scenario 'they see snack stats' do
     sam = Owner.create(name: "Sam's Snacks")
     vm1 = sam.machines.create(location: "Don's Mixed Drinks")
+    vm1.snacks.create(name: 'Scooby Snacks', price: 20)
     vm2 = sam.machines.create(location: "Batsnacks")
     snack = Snack.create(name: 'Yummy Gummy', price: 10)
 
     vm1.snacks << snack
     vm2.snacks << snack
 
+    string1 = "* #{vm1.location} (#{vm1.snacks.size} kinds of snacks, average price of #{vm1.average_snack_price})"
+    string2 = "* #{vm2.location} (#{vm2.snacks.size} kinds of snacks, average price of #{vm2.average_snack_price})"
+    
     visit snack_path(snack)
 
     expect(page).to have_content(vm1.location)
     expect(page).to have_content(vm2.location)
     expect(page).to have_content(snack.price)
+    expect(page).to have_content(string1)
+    expect(page).to have_content(string2)
     expect(page).to have_content(vm1.average_snack_price)
     expect(page).to have_content(vm2.average_snack_price)
     expect(page).to have_content(vm1.snacks.size)
